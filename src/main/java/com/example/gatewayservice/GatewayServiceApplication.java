3729df96146c551d419cb104f9d2e0e343c7f121
Andersen <mizio.spatafora@gmail.com>
2021-07-24 12:57:47 +0200

    Implement restricted access such that all GET request now gets send to microservice on port 8082 while POST requests are sent to 8081

--- src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,23 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("path_route", r -> r.path("/greeting").and().method("POST").uri("http://localhost:8081"))
+				.route("path_route", r -> r.path("/greeting/**").and().method("GET").uri("http://localhost:8082"))
+				.build();
 	}
 
 }
