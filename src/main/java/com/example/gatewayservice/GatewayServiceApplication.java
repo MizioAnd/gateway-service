7871cdf421316c78b5265db6b3ff569cd4093f12
Andersen <mizio.spatafora@gmail.com>
2021-07-31 13:24:07 +0200

    Implement dependency and Bean for swagger

--- src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
@@ -20,6 +25,12 @@ public class GatewayServiceApplication {
 				.route("path_route", r -> r.path("/greeting/**").and().method("GET").filters(f -> f.addResponseHeader("Cache-Control", "max-age=300"))
 						.uri("http://localhost:8082"))
 				.build();
+	}
+
+	@Bean
+	public Docket greetingAPI() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.gatewayservice")).build();
 	}
 
 }
